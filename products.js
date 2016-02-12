5b7d39ca2b3bac2dc95b0f1e802875182f305e14
ZachParris <zacharyjparris@gmail.com>
2016-02-11 20:44:13 -0600

    products html/js

---
+++ products.js
@@ -0,0 +1,42 @@
+var products= [
+	["beachSelfie", "Getting that sand out of your bathing suit","$4","img/"],
+	["skyScraper", "We'll catch you on the edge", "$6", "img/"],
+	["groupSelfie", "Show off your better looking friends", "$.25", "img/"],
+	["vacationSelfie", "Dont worry i'll find you", "$5 + airfare", "img/"],
+	["concertSelfie", "We'll steal your spotlight", "$3 + ticket price", "img/"],
+	["diySelfie", "supervised selfie BYOSS", "$27", "img/"],
+	["vanSelfie", "free candy", "free", "img/"],
+	["bathroomSelfie", "you'll never know", "$12", "img/"]
+	];
+
+
+var productLeft= document.getElementById("left")
+var productRight= document.getElementById("right")
+var productList=""
+var productGroup=""
+
+
+for (var i=0; i<products.length/2; i++) {
+	productList+= "<section>"
+	productGroup+= "<section>"
+
+	productList += "<img src= '" + products[i][3] + "'>"
+	productGroup += "<img src='" + products[i + 4][3] + "'>"
+	productList += "<strong>" + products[i][0] + "</strong>"
+	productGroup += "<strong>" + products[i + 4][0] + "</strong>"
+	productList += "<p>" + products[i][1] + "</p>"
+	productGroup += "<p>" + products[i + 4][1] + "</p>"
+	productList += "<p>" + products[i][2] + "</p>"
+	productGroup += "<p>" + products[i + 4][2] + "</p>"
+
+	productList += "</section>"
+	productGroup += "</section>"
+
+}
+console.log(productList)
+console.log(productGroup)
+
+
+productLeft.innerHTML = productList
+productRight.innerHTML = productGroup
+
